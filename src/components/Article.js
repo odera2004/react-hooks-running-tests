1f76d9dd7a7fb15a12a9b1314d5dbf1827323163
odera <eugeneodera59@gmail.com>
2024-10-30 12:23:57 +0300

    add text

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,22 @@
+import { render, screen } from '@testing-library/react';
 import React from "react";
 
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+      please pass this test
     </div>
   );
 }
 
 export default Article;
+
+test("displays the text 'please pass this test'", () => {
+  render (<Article />);
+
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug(element);
+
+  expect(element).toBeInTheDocument();
+});
